b209471c7a12899792c86dfb2b4dea2d1c9adf11
janesh-kiddev <officialjanesh12@gmail.com>
2025-04-01 23:13:12 +0530

    added predictions.py  and output

---
+++ predictions.py
@@ -0,0 +1,21 @@
+from ultralytics import YOLO
+import os
+
+model = YOLO(r"C:\\Ml projects\\Pothole\\Pothole-Detection\\best.pt")  
+
+results = model([
+    r"C:\\Ml projects\\Pothole\\Pothole-Detection\\image1.jpeg",
+    r"C:\\Ml projects\\Pothole\\Pothole-Detection\\image2.jpeg",
+    r"C:\\Ml projects\\Pothole\\Pothole-Detection\\images3.jpeg"
+    ]) 
+
+
+for id,result in enumerate(results):
+    boxes = result.boxes  
+    masks = result.masks  
+    keypoints = result.keypoints  
+    probs = result.probs  
+    obb = result.obb  
+    # result.show()  
+    output = os.path.join("Pothole-Detection\output",f"result{id+1}.jpg")  
+    result.save(output)
